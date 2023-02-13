dad2201a20cd9da56a354e2c8c41e3003741bbe2
SandipPandhare <93873281+SandipPandhare@users.noreply.github.com>
2023-02-13 22:48:30 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    int iCnt=1;
+    while(iCnt<5)
+    {
+        cout<<"Marvellous"<<"\n";
+        iCnt++;
+    }
+}
+int main()
+{
+    Display();   
+    return 0;
+}
